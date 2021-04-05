bafcf167c767bb3f279839cfaa2fdf9e6d837ed8
Saicod <46506916+Saicod@users.noreply.github.com>
2021-04-05 04:57:50 +0000

    Deploying to gh-pages from  @ 7096c70896998dfbfbfecfc928bec8464cee8790 🚀

--- docs/sketches/team/acastillos/ilusion.js
+++ docs/sketches/team/acastillos/ilusion.js
@@ -1,1 +1,39 @@
-let img;
+let rotating = false;
+let dissapear = false;
+let a = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  translate(200, 200);
+  rotate(a);
+  if (rotating) {
+    a += PI/4;
+  }
+  fill(255, 0, 0);
+  noStroke();
+  arc(0, 0, 300, 300, 0, PI/4);
+  arc(0, 0, 300, 300, PI/2, 3*PI/4);
+  arc(0, 0, 300, 300, PI, 5*PI/4);
+  arc(0, 0, 300, 300, 3*PI/2, 7*PI/4);
+  if (dissapear) {
+    fill(0, 255, 255);
+  } else {
+    fill(0, 255, 0);
+  }
+  arc(0, 0, 300, 300, PI/4, PI/2);
+  arc(0, 0, 300, 300, 3*PI/4, PI);
+  arc(0, 0, 300, 300, 5*PI/4, 3*PI/2);
+  arc(0, 0, 300, 300, 7*PI/4, TWO_PI);
+}
+
+function mousePressed() {
+  if (mouseButton == LEFT) {
+    rotating = !rotating;
+  } else if (mouseButton == RIGHT) {
+    dissapear = !dissapear;
+  }
+}
